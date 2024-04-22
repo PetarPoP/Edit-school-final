4d0ff650beb613d7a87907304707b5bb1b6a20cc
pop <petarpopovic719@gmail.com>
2024-04-22 10:51:48 +0200

    Changes by omznc

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { Outlet } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="flex flex-col items-center min-h-screen h-full max-h-screen w-full">
-      <div className="sticky top-0 w-full bg-background z-50">
-        <div className="flex-row flex h-16 items-center px-4 w-full justify-between border-b">
+    <div className="flex flex-col items-center dvh w-full">
+      <div className="sticky top-0 w-full px-4 pt-4 z-50">
+        <div className="flex-row flex h-16 items-center rounded-md px-4 w-full justify-between border bg-white/80 backdrop-blur">
           <Nav />
         </div>
       </div>
-      <div className="w-full flex p-8 justify-center">
-        <div className="w-full max-w-[1200px] flex flex-col gap-8">
+      <div className="w-full flex p-8 pt-16 justify-center">
+        <div className="w-full flex flex-col gap-8">
           <Outlet />
         </div>
       </div>
--- src/components/radionica-card.tsx
+++ src/components/radionica-card.tsx
@@ -1,25 +1,47 @@
 import useSWR from "swr";
 import { fetcher } from "@/lib/utils.ts";
+import { Button } from "@/components/ui/button.tsx";
+import { Link } from "react-router-dom";
 
 export function RadionicaCard({ radionica }: { radionica: Workshop }) {
   const { data, error, isLoading } = useSWR(
     "http://localhost:3000/presenters",
     fetcher,
   );
+
   if (error) return <div>Problem jbggggggg</div>;
   if (isLoading) return <div>Loading</div>;
 
-
   return (
-    <div className="flex flex-col">
+    <div className="flex border rounded-md overflow-hidden bg-white transition-all w-full">
       <div>
-        <img src={radionica.image} alt="Building" />
+        <img src={radionica.image} alt="Building" className="border" />
       </div>
-      <div className="flex flex-col">
-        <h1>{radionica.title}</h1>
-        <p>{radionica.description}</p>
-        <p>Predavač: {data.filter((presenter: Presenter) => radionica.presenterIds
-        <button>Prijavi se</button>
+      <div className="flex p-4 gap-2 flex-col justify-between items-start">
+        <div className="flex gap-2 flex-col justify-start items-start">
+          <h1 className="text-2xl font-semibold">{radionica.title}</h1>
+          <p>{radionica.description}</p>
+          <p className="flex gap-1">
+            <span>Predavač(i):</span>
+            {(data as Presenter[])
+              .filter((presenter) =>
+                radionica.presenterIds.includes(presenter.id),
+              )
+              .map((presenter) => (
+                <Link
+                  to={`/predavaci/${presenter.id}`}
+                  key={`${presenter.id}-${radionica.id}`}
+                  className="bg-gray-200 px-1 py-0.5 hover:bg-gray-300 transition-all rounded-md border"
+                >
+                  {presenter.name}
+                </Link>
+              ))}
+          </p>
+        </div>
+        <div className="flex gap-2">
+          <Button>Prijavi se</Button>
+          <Button variant="secondary">Uredi</Button>
+        </div>
       </div>
     </div>
   );
